4fe6a14a1bf4c8e7ca39c2b9cfad0123fc546295
catttam <caterinaalarconm@gmail.com>
2024-07-16 10:38:37 +0200

    Fixed paths

--- extract_goaccess_metrics.sh
+++ extract_goaccess_metrics.sh
@@ -1,8 +1,7 @@
 # Container paths
 OUTPUTS_PATH="/app/metrics/rawmetrics"
 FILTERED_PATH="$OUTPUTS_PATH/filtered_by_status_code"
-OUTPUT_FILE="output"
-
+OUTPUT_FILE="output_OSCAR_goaccess"
 # Path to the readonly volume with the cluster's ingress logs
 CLUSTER_LOGS_DIR="/var/log/clusterlogs"
 LOCAL_LOGS_DIR="/var/log/ingresslogs"
@@ -21,13 +20,13 @@ addLog(){
 
 metrics(){
 
-    geo_err=$( { goaccess "${FULL_REPORT_FILE}" --log-format="${LOG_FORMAT}" -o "${OUTPUTS_PATH}/${OUTPUT_FILE}_${FULL_REPORT_FILE}.json" --json-pretty-print; } 2>&1 )
-    python3 goaccess_metric_parser.py -f "${OUTPUTS_PATH}/${OUTPUT_FILE}_${FULL_REPORT_FILE}.json" -g 0
+    geo_err=$( { goaccess "${FULL_REPORT_FILE}" --log-format="${LOG_FORMAT}" -o "${OUTPUTS_PATH}/${OUTPUT_FILE}_full.json" --json-pretty-print; } 2>&1 )
+    python3 goaccess_metric_parser.py -f "${OUTPUTS_PATH}/${OUTPUT_FILE}_full.json" -g 0
 
     status_codes=('200' '204' '404' '500')
     init="t"
 
-    out="${FILTERED_PATH}/${OUTPUT_FILE}_${FULL_REPORT_FILE}"
+    out="${FILTERED_PATH}/${OUTPUT_FILE}"
 
     for code in "${status_codes[@]}"; do
     code_logs=$(cat $FULL_REPORT_FILE| grep -e 'HTTP/[0-9].[0-9]" '${code}' ')
