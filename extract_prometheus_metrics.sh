cdf83402911ec9a7a94c8547e650ee33a8fe87bb
catttam <caterinaalarconm@gmail.com>
2024-10-21 12:41:04 +0200

    Fixed prometheus filename error

--- extract_prometheus_metrics.sh
+++ extract_prometheus_metrics.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cluster_auth='{"cluster_id":"'"${CLUSTER_ID}"'","endpoint":"'"${ENDPOINT}"'","user":"'"${USER}"'","password":"'"${PASSW}"'","ssl":"True"}'
-python3 metrics_prom.py $PROMETHEUS_ENDPOINT $VO $cluster_auth
+python3 prometheus_metric_parser.py $PROMETHEUS_ENDPOINT $VO $cluster_auth
