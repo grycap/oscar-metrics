4448d85d8ce9b1892eb6d2de49f9d56e93dec111
catttam <caterinaalarconm@gmail.com>
2024-10-23 11:30:51 +0200

    Adapted output to Grafana

--- extract_logs_metrics.sh
+++ extract_logs_metrics.sh
@@ -1,17 +1,15 @@
 # Container paths
 OUTPUTS_PATH="/app/metrics/rawmetrics"
 FILTERED_PATH="$OUTPUTS_PATH/filtered_by_status_code"
-OUTPUT_FILE="output_OSCAR_goaccess"
+
 # Path to the readonly volume with the cluster's ingress logs
 CLUSTER_LOGS_DIR="/var/log/clusterlogs"
 LOCAL_LOGS_DIR="/var/log/ingresslogs"
 OSCAR_LOGS_DIR="$LOCAL_LOGS_DIR/oscar"
 
 HISTORY_LOGS_INFERENCE="$OSCAR_LOGS_DIR/inference/oscar.log"
-LATEST_LOGS_INFERENCE="$OSCAR_LOGS_DIR/inference/latest_oscar.log"
-
 HISTORY_LOGS_CREATE="$OSCAR_LOGS_DIR/create/oscar.log"
-LATEST_LOGS_CREATE="$OSCAR_LOGS_DIR/create/latest_oscar.log"
+
 mkdir -p $OSCAR_LOGS_DIR/inference
 mkdir -p $OSCAR_LOGS_DIR/create
 
@@ -21,18 +19,14 @@ LOG_FORMAT='%^ %^ %^ %^ [%^] %d - %t | %s | %Ts | %h | %m %~ %U | %u'
 addLog(){
     ingress_logfile=$1
     cat $ingress_logfile | grep GIN-EXECUTIONS-LOGGER | grep -a '/job\|/run' | tee -a $HISTORY_LOGS_INFERENCE >/dev/null
-    cat $logfile | grep CREATE-HANDLER | grep '/system/services' | tee -a $HISTORY_LOGS_INFERENCE >/dev/null
+    cat $logfile | grep CREATE-HANDLER | grep '/system/services' | tee -a $HISTORY_LOGS_CREATE >/dev/null
 }
 
 metrics(){
     LOG_FILE=$1
     filename=`basename "$LOG_FILE"`
     geo_err=$( { goaccess "${LOG_FILE}" --log-format="${LOG_FORMAT}" -o "${OUTPUTS_PATH}/${filename}_full.json" --json-pretty-print; } 2>&1 )
-    if [[ $filename == "latest"* ]]; then
-        python3 logs_metric_parser.py -f "${OUTPUTS_PATH}/${filename}_full.json" -g 0
-    else
-        python3 logs_metric_parser.py -f "${OUTPUTS_PATH}/${filename}_full.json" -g 0 -u
-    fi
+    python3 logs_metric_parser.py -f "${OUTPUTS_PATH}/${filename}_full.json" -g 0
     
     status_codes=('200' '201' '204' '404' '500')
     init="t"
@@ -58,7 +52,7 @@ metrics(){
 }
 
 
-
+# Read current logs and upload backup to s3 
 for log_path in "$CLUSTER_LOGS_DIR"/*;
 do
     if [[ $log_path == *"oscar_oscar"* ]]; then
@@ -67,13 +61,6 @@ do
         relative_log_path=$(echo $log_path | sed 's/\/var\/log\/clusterlogs\///')
         # Upload current logs to s3
         aws s3 cp --recursive $log_path s3://metrics.oscar.grycap.net/"${CLUSTER_ID}"/ingresslogs/"${relative_log_path}"
-        for logfile in "$LOCAL_LOGS_DIR/$relative_log_path/oscar/"*;
-        do
-            if [[ $logfile == *".gz" ]]; then
-                # unzip all log files
-                gzip -d $logfile
-            fi
-        done
         break
     fi
 done
@@ -84,34 +71,26 @@ aws s3 cp s3://metrics.oscar.grycap.net/"${CLUSTER_ID}"/ingresslogs $LOCAL_LOGS_
 # /var/log/ingresslogs/oscar_oscar-7499cd/oscar
 for logfile in "$LOCAL_LOGS_DIR/oscar_oscar"*"/oscar/"*;
 do
-    if [[ $logfile == *".log"* ]]; then
-        if [[ $logfile == *".log" ]]; then
-            echo ">> Filtering last logfile '$logfile'"
-            cat $logfile | grep GIN-EXECUTIONS-LOGGER | grep -a '/job\|/run' | tee -a $LATEST_LOGS_INFERENCE >/dev/null
-            cat $logfile | grep CREATE-HANDLER | grep '/system/services' | tee -a $LATEST_LOGS_CREATE >/dev/null
-            metrics $LATEST_LOGS_INFERENCE
-        else
-            echo ">> Adding log '$logfile'"
+    if [[ $logfile == *".gz" &&  ]]; then
+        # unzip all log files
+        gzip -d $logfile
+        logfile=$(echo $logfile | sed 's/\.gz//')
+    fi
+
+    if [[ $logfile == *".log"* || $logfile == *".log" ]]; then
+            echo ">> Adding logfile '$logfile'"
             addLog $logfile
-        fi
     fi
 done
 
-# Generate the html file
-if [ ! -f "${HISTORY_LOGS_INFERENCE}" ] || [ ! -s "${HISTORY_LOGS_INFERENCE}" ]; then
-    goaccess "${LATEST_LOGS_INFERENCE}" --log-format="${LOG_FORMAT}" -o "/app/metrics/dashboard.html"
-    python3 logs_metric_parser.py -f $LATEST_LOGS_INFERENCE -i 0
-else
-    metrics $HISTORY_LOGS_INFERENCE
+awk '!seen[$0]++' $HISTORY_LOGS_INFERENCE > tmp_inference.log
+mv tmp_inference.log $HISTORY_LOGS_INFERENCE
 
-    cat $LATEST_LOGS_INFERENCE | tee -a $HISTORY_LOGS_INFERENCE >/dev/null
-    python3 logs_metric_parser.py -f $HISTORY_LOGS_INFERENCE -i 0 -u
-    goaccess "${HISTORY_LOGS_INFERENCE}" --log-format="${LOG_FORMAT}" -o "/app/metrics/dashboard.html"
+awk '!seen[$0]++' $HISTORY_LOGS_CREATE > tmp_create.log
+mv tmp_create.log $HISTORY_LOGS_CREATE
 
-fi
+metrics $HISTORY_LOGS_INFERENCE
+python3 logs_metric_parser.py -f $HISTORY_LOGS_INFERENCE -i 0
+python3 logs_metric_parser.py -f $HISTORY_LOGS_CREATE -c 0
 
-if [ ! -f "${HISTORY_LOGS_CREATE}" ] || [ ! -s "${HISTORY_LOGS_CREATE}" ]; then
-    python3 logs_metric_parser.py -f $LATEST_LOGS_CREATE -c 0
-else
-    python3 logs_metric_parser.py -f $HISTORY_LOGS_CREATE -c 0 -u
-fi
+rm tmp_*.log
